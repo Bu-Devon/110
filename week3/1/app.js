914c6613cc6cc4365f6b4262ad72bcfaccc50076
Bu-Devon <devon.brown@bryanuniversity.edu>
2021-03-06 22:09:23 -0800

    DOM

---
+++ week3/1/app.js
@@ -0,0 +1,29 @@
+{
+var h1 = document.createElement('h1');
+h1.textContent = 'HelloWorld';
+document.body.append(h1);
+} 
+
+// loops
+
+for (i = 0; i < 9; i++){
+    console.log (h1 = document.createElement('h1'),
+     h1.textContent = 'HelloWorld',
+     document.body.append (h1),
+     document.getElementsByTagName("h1")[0].setAttribute("class", "vas"),
+     
+     )
+}
+{
+var ul = document.createElement('ul');
+document.body.append(ul);
+document.getElementsByTagName("ul")[0].setAttribute("id", "list"); 
+}
+
+{
+const names = ['steve', 'larry', 'joe', 'shirley', 'steph', 'nate', 'rick', 'emily'];
+
+for (i = 0; i < names.length; i++){
+    console.log( ul.innerHTML += '<li>' + names[i] + '</li>')
+}
+}
