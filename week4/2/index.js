1a6e98ee7d1d3cd62e4238bc7b48f07887b55706
Bu-Devon <devon.brown@bryanuniversity.edu>
2021-03-27 07:10:23 -0700

    DOM

--- week4/2/index.js
+++ week4/2/index.js
@@ -1,42 +1,40 @@
-let submitArray = [];
+var enterArray = [];
+var operationNames = ["Add", "Sub", "Multiply"];
 
-let colors = ["red", "blue", "green"];
-let text = ["Addition", "Subtraction", "Multiplication"];
 
-for(let i = 0; i < 3; i++) {
-    let div = document.createElement("div");
+for(i = 0; i < 3; i++) {
+    var div = document.createElement("div");
     div.style.display = "block";
 
-    let input1 = document.createElement('input');
+    var input1 = document.createElement('input');
     input1.id = "first" + i.toString();
-    input1.style.margin = "15px";
-    input1.style.padding = "4px";
-
-    let input2 = document.createElement('input');
-    input2.id = "second" + i.toString();
-    input2.style.margin = "15px";
-    input2.style.padding = "4px";
-
-    let submit = document.createElement('input');
-    submit.style.margin = "15px";
-    submit.style.padding = "4px";
-    submit.type = "submit";
-    submitArray.push(submit);
-
-    let description = document.createElement('span');
-    description.style.margin = "15px";
-    description.style.fontSize = "20px";
-    description.textContent = text[i];
-    description.style.color = colors[i];
-
+    input1.style.margin = "10px";
+    input1.style.padding = "2px";
     div.appendChild(input1);
+
+    var input2 = document.createElement('input');
+    input2.id = "second" + i.toString();
+    input2.style.margin = "10px";
+    input2.style.padding = "2px";
     div.appendChild(input2);
-    div.appendChild(submit);
-    div.appendChild(description);
+
+    var enter = document.createElement('input');
+    enter.style.margin = "10px";
+    enter.style.padding = "2px";
+    enter.type = "submit";
+   enterArray.push(enter);
+   div.appendChild(enter);
+
+    var operations = document.createElement('p');
+    operations.style.margin = "20px";
+    operations.style.fontSize = "30px";
+    operations.textContent = operationNames[i];
+    div.appendChild(operations);
     document.body.appendChild(div);
 }
 
-let answer = document.createElement("span");
+
+var answer = document.createElement("p");
 answer.id = "answer";
 answer.textContent = "Answer: ";
 answer.style.margin = "15px";
@@ -44,34 +42,34 @@ answer.style.fontSize = "20px";
 
 document.body.appendChild(answer);
 
-submitArray[0].addEventListener("click", (Event) => {
-    let input1 = document.getElementById("first0");
-    let input2 = document.getElementById("second0");
-    let answer = document.getElementById("answer");
+enterArray[0].addEventListener('click', (e) => {
+    var input1 = document.getElementById("first0");
+    var input2 = document.getElementById("second0");
+    var answer = document.getElementById("answer");
 
-    answer.textContent = "Answer: " + (parseInt(input1.value) + parseInt(input2.value));
-
-    input1.value = "";
-    input2.value = "";
-});
-
-submitArray[1].addEventListener('click', (Event) => {
-    let input1 = document.getElementById("first1");
-    let input2 = document.getElementById("second1");
-    let answer = document.getElementById("answer");
-
-    answer.textContent = "Answer: " + (parseInt(input1.value) - parseInt(input2.value));
+    answer.textContent =  + (parseInt(input1.value) + parseInt(input2.value));
 
     input1.value = "";
     input2.value = "";
 });
 
-submitArray[2].addEventListener('click', (Event) => {
-    let input1 = document.getElementById("first2");
-    let input2 = document.getElementById("second2");
-    let answer = document.getElementById("answer");
+enterArray[1].addEventListener('click', (e) => {
+    var input1 = document.getElementById("first1");
+    var input2 = document.getElementById("second1");
+    var answer = document.getElementById("answer");
 
-    answer.textContent = "Answer: " + (parseInt(input1.value) * parseInt(input2.value));
+    answer.textContent =  + (input1.value) - (input2.value);
+
+    input1.value = "";
+    input2.value = "";
+});
+
+enterArray[2].addEventListener('click', (e) => {
+    var input1 = document.getElementById("first2");
+    var input2 = document.getElementById("second2");
+    var answer = document.getElementById("answer");
+
+    answer.textContent =  (input1.value) * (input2.value);
 
     input1.value = "";
     input2.value = "";
